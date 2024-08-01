20bf9454b8f35725b56beb4bc4100925072d5b7b
Aleeza Rubab <167331549+Aleeza-12@users.noreply.github.com>
2024-08-01 14:15:39 +0500

    navbar changes

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = ({ onLoginClick, onSignupClick }) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [profile, setProfile] = useState({
+    name: 'Aleeza Rubab',
+    email: 'malikaleeza1122@gmail.com',
+    image: '/images/SAlogo.jpg', // Placeholder image path
+  });
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const handleSignOut = () => {
+    console.log('Sign out');
+  };
+
+  const handleProfileChange = (newProfile) => {
+    setProfile(newProfile);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
@@ -11,9 +30,21 @@ const Navbar = ({ onLoginClick, onSignupClick }) => {
       <div className="navbar-right">
         <button className="nav-button" onClick={onLoginClick}>Login</button>
         <button className="nav-button" onClick={onSignupClick}>Signup</button>
-        <div className="profile">
-          <img src={'/images/SAlogo.jpg'} alt="Profile" className="icon" />
-          <span>Amanda Smith</span>
+        <div className="profile" onClick={toggleDropdown}>
+          <img src={profile.image} alt="Profile" className="icon" />
+          <span>{profile.name}</span>
+          {dropdownOpen && (
+            <div className="dropdown-menu">
+              <div className="dropdown-header">
+                <img src={profile.image} alt="Profile" className="dropdown-icon" />
+                <div>
+                  <div className="dropdown-name">{profile.name}</div>
+                  <div className="dropdown-email">{profile.email}</div>
+                </div>
+              </div>
+              <div className="dropdown-item" onClick={handleSignOut}>Sign Out</div>
+            </div>
+          )}
         </div>
       </div>
     </nav>
